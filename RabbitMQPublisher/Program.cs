20170a2e1068131b8b99ea2b363d57cb2601a052
Dilara Nur Altıparmak <78371701+dilaranuraltiparmak@users.noreply.github.com>
2023-05-18 03:32:36 +0300

    Add project files.

---
+++ RabbitMQPublisher/Program.cs
@@ -0,0 +1,14 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+ConnectionFactory factory = new();
+factory.Uri=new("amqps://feayvied:AHFwelZWAxbfuja1ZdAaQ2zal4Y39ij0@cow.rmq2.cloudamqp.com/feayvied");
+using IConnection connection
+    = factory.CreateConnection();
+using IModel channel= connection.CreateModel();
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+byte[] message=Encoding.UTF8.GetBytes("Selam");
+channel.BasicPublish(exchange:"",routingKey:"example-queue",body:message);
+Console.Read();
+
